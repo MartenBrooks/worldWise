86fe2c6826b17c6439a7ee197dc17f1cc1fd8648
Yuriy Tarasevich <marten.ao.ns@gmail.com>
2024-01-18 23:38:08 +0300

    fourth commit

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -2,17 +2,18 @@ import CityItem from './CityItem';
 import styles from './CityList.module.css';
 import Spinner from './Spinner';
 import Message from './Message.jsx';
-function CityList({ cities, isLoading }) {
+import { useCities } from '../contexts/CitiesContext.jsx';
+
+function CityList() {
+  const { cities, isLoading } = useCities();
+
   if (isLoading) return <Spinner />;
 
-  if (!cities.length) return <Message message="No cities" />;
+  if (!cities.length) return <Message message='No cities' />;
   return (
     <ul className={styles.cityList}>
       {cities.map((city, i) => (
-        <CityItem
-          key={city.id}
-          city={city}
-        />
+        <CityItem key={city.id} city={city} />
       ))}
     </ul>
   );
--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -2,10 +2,13 @@ import styles from './CountryList.module.css';
 import CountryItem from './CountryItem.jsx';
 import Message from './Message';
 import Spinner from './Spinner';
+import { useCities } from '../contexts/CitiesContext.jsx';
 
-function CountryList({ cities, isLoading }) {
+function CountryList() {
+  const { cities, isLoading } = useCities();
+
   if (isLoading) return <Spinner />;
-  if (!cities.length) return <Message message="No cities" />;
+  if (!cities.length) return <Message message='No cities' />;
   const countries = cities.reduce((acc, city) => {
     if (!acc.map((a) => a.country).includes(city.country)) {
       return [
@@ -18,10 +21,7 @@ function CountryList({ cities, isLoading }) {
   return (
     <ul className={styles.countryList}>
       {countries.map((country, i) => (
-        <CountryItem
-          country={country}
-          key={country.id}
-        />
+        <CountryItem country={country} key={country.id} />
       ))}
     </ul>
   );
